ed467352de143b9e37f369f5d1ce007ba0ab2545
ssbarbee <ssbarbee@users.noreply.github.com>
2024-03-09 21:22:28 +0100

    feat: use openmeteo for weather

---
+++ src/index.ts
@@ -0,0 +1,79 @@
+import { render } from 'mustache';
+import fetch from 'node-fetch';
+import { readFile, writeFileSync } from 'fs';
+import { getWeather } from './services/weather';
+
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+interface Data {
+  refreshTime: string;
+  city_temperature?: string;
+  feelsLike?: string;
+  weatherDescription?: string;
+  humidity?: string;
+  sunRise?: string;
+  sunSet?: string;
+  pm10?: string;
+  pm25?: string;
+}
+
+const DATA: Data = {
+  refreshTime: new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Europe/Skopje',
+  }),
+};
+
+async function setWeatherInformation(): Promise<void> {
+  const {
+    current: {
+      temperature2m: temp,
+      apparentTemperature: feelsLike,
+      description,
+      relativeHumidity2m: humidity,
+    },
+    daily: { sunset, sunrise },
+  } = await getWeather();
+
+  const defaultValue = '---';
+  DATA.city_temperature = !temp ? defaultValue : Math.round(temp).toString();
+  DATA.feelsLike = !feelsLike ? defaultValue : Math.round(feelsLike).toString();
+  DATA.weatherDescription = description || defaultValue;
+  DATA.humidity = humidity.toString() || defaultValue;
+  DATA.sunRise = sunrise ? sunrise : defaultValue;
+  DATA.sunSet = sunset ? sunset : defaultValue;
+}
+
+async function setPollutionData(): Promise<void> {
+  try {
+    const response = await fetch(`https://skopje.pulse.eco/rest/overall`);
+    const data = (await response.json()).values;
+
+    DATA.pm10 = data.pm10 + ' μg/m3';
+    DATA.pm25 = data.pm25 + ' μg/m3';
+  } catch (_) {
+    DATA.pm10 = 'Not available';
+    DATA.pm25 = 'Not available';
+  }
+}
+
+function generateReadMe(): void {
+  readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = render(data.toString(), DATA);
+    writeFileSync('README.md', output);
+  });
+}
+
+async function action(): Promise<void> {
+  await setWeatherInformation();
+  await setPollutionData();
+  generateReadMe();
+}
+
+void action();
---
+++ src/services/weather.ts
@@ -0,0 +1,78 @@
+function mapWeatherCodeToDescription(code: number): string {
+  switch (code) {
+    case 0:
+      return 'Clear sky';
+    case 1:
+    case 2:
+    case 3:
+      return 'Mainly clear, partly cloudy, and overcast';
+    case 45:
+    case 48:
+      return 'Fog and depositing rime fog';
+    case 51:
+    case 53:
+    case 55:
+      return 'Drizzle: Light, moderate, and dense intensity';
+    case 56:
+    case 57:
+      return 'Freezing Drizzle: Light and dense intensity';
+    case 61:
+    case 63:
+    case 65:
+      return 'Rain: Slight, moderate and heavy intensity';
+    case 66:
+    case 67:
+      return 'Freezing Rain: Light and heavy intensity';
+    case 71:
+    case 73:
+    case 75:
+      return 'Snow fall: Slight, moderate, and heavy intensity';
+    case 77:
+      return 'Snow grains';
+    case 80:
+    case 81:
+    case 82:
+      return 'Rain showers: Slight, moderate, and violent';
+    case 85:
+    case 86:
+      return 'Snow showers slight and heavy';
+    case 95:
+      return 'Thunderstorm: Slight or moderate';
+    case 96:
+    case 99:
+      return 'Thunderstorm with slight and heavy hail';
+    default:
+      return 'Unknown weather condition';
+  }
+}
+
+export const getWeather = async () => {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=42&longitude=21.42&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,surface_pressure,wind_speed_10m,wind_direction_10m&daily=sunrise,sunset&timezone=Europe%2FSkopje`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const { current, daily } = data;
+
+  const weatherData = {
+    current: {
+      temperature2m: current.temperature_2m,
+      relativeHumidity2m: current.relative_humidity_2m,
+      apparentTemperature: current.apparent_temperature,
+      isDay: current.is_day === 1,
+      precipitation: current.precipitation,
+      rain: current.rain,
+      showers: current.showers,
+      snowfall: current.snowfall,
+      weatherCode: current.weather_code,
+      surfacePressure: current.surface_pressure,
+      windSpeed10m: current.wind_speed_10m,
+      windDirection10m: current.wind_direction_10m,
+      description: mapWeatherCodeToDescription(current.weather_code),
+    },
+    daily: {
+      sunrise: daily.sunrise[0].split('T')[1],
+      sunset: daily.sunset[0].split('T')[1],
+    },
+  };
+
+  return weatherData;
+};
